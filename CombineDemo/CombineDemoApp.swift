0f4f824eec06d5a1ea8af6f05ac69c781d05de17
Chouson <532095303@qq.com>
2024-01-19 16:12:47 +0800

    Initial Commit

---
+++ CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by Chouson on 2024/1/19.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
